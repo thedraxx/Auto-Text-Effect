e1938864ebf59c39d0319f51509b3039a6eaebe5
Thedraxx <thedraxxgames@gmail.com>
2022-03-16 14:34:03 -0300

    finish

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const textin = document.getElementById('textin');
+const enter = document.getElementById('submit');
+
+let text = "Hello World!!!";
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+   
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(() => {
+        writeText();
+    }, speed);
+}
+
+speedEl.addEventListener('input', (e) => {
+    speed = 300 / e.target.value;
+})
+
+enter.addEventListener('click', (e) => {
+    text = textin.value;
+})
